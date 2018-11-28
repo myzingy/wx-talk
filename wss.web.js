064830d69d4c5ccec193e057b386f01ce2d71896
vking <goto999@126.com>
2018-11-28 22:16:45 +0800

    del

--- wss.web.js
+++ wss.web.js
@@ -51,9 +51,10 @@ wsServer.on('connect', connection => {
         talk.mp3(file,'wav').then(filename=>{
           //connection.sendUTF('reply:wav~~'+filename);
           talk.baiduApi(filename,'A'+Math.random()).then(res=>{
-            res=JSON.stringify(res);
-            console.log('baiduApi.res:',res)
-            connection.sendUTF(res);
+            if(res.result){
+              console.log('baiduApi.res:',res.result)
+              connection.sendUTF(res.result);
+            }
           });
         })
       });
